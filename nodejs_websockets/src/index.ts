85f3ad9631fae96879d3c5670c59e6dbaa106455
Deep <diphiwase0987@gmail.com>
2025-04-29 12:58:46 +0530

    Add Broadcast Message code -Add Connection id creation logic

--- nodejs_websockets/src/index.ts
+++ nodejs_websockets/src/index.ts
@@ -5,13 +5,28 @@ const wss = new WebSocketServer({ port: 8080 });
 
 wss.on('connection', function(socket) {
   socket.on('error', console.error)
-  console.log("Connection established! " + Math.random())
+  let connectionID = Math.random()
+  console.log("Connection established! " + connectionID)
 
   socket.on('message', (e) => {
-    if (e.toString().toLowerCase() === "ping") {
-      socket.send("pong");
-    } else {
-      socket.send(e.toString());
-    }
+    wss.clients.forEach((client) => {
+      if (client !== socket && client.readyState === WebSocket.OPEN) {
+        if (e.toString().toLowerCase() === "ping") {
+          client.send("pong");
+        } else {
+          client.send(e.toString());
+        }
+      }
+    })
+    // if (e.toString().toLowerCase() === "ping") {
+    //   socket.send("pong");
+    // } else {
+    //   socket.send(e.toString());
+    // }
   })
+
+  socket.on('close', () => {
+    console.log("Client disconnected " + connectionID)
+  })
+  
 })
